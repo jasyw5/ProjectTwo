8d941a57e4e44c7bc3f710edef1c1658f2c2a767
John Stahl <john.stahl24@gmail.com>
2018-08-15 03:18:09 +0000

    added the latitude and longistude

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180803011722) do
+ActiveRecord::Schema.define(version: 20180815030528) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180803011722) do
     t.datetime "updated_at",  null: false
     t.string   "hours"
     t.integer  "user_id"
+    t.float    "latitude"
+    t.float    "longitude"
     t.index ["user_id"], name: "index_places_on_user_id", using: :btree
   end
 
